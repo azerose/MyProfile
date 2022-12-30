989c0b2f40cbfb54c3395eef400ebc3cb25dae26
azerose <woojjang8671@naver.com>
2022-12-30 17:23:47 +0900

    HOTFIX: 배포시 에러해결

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  trailingSlash: true,
+  generateBuildId: () => "myprofile-jwl",
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/market": { page: "/market" },
+    "/404": { page: "/404" },
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
